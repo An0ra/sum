2716af1d5cd8ce9fa9c566f66dc85ff704185472
devweb <devweb.chronomed@gmail.com>
2022-08-29 10:31:08 -0400

    Starting Project

---
+++ sum/sum/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int val1;
+	int val2;
+
+
+	cout << "Entrer deux valeurs à additionner" << endl;
+	cin >> val1 >> val2;
+	cout<< " = " << val1 + val2;
+
+	return 0;
+}
